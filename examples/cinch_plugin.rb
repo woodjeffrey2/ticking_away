445047e6d9b76d9e599871d103a9843fa2277902
Jeff Wood <woodjeffrey2@gmail.com>
2021-05-09 13:16:34 -0700

    fix example and README

--- examples/cinch_plugin.rb
+++ examples/cinch_plugin.rb
@@ -2,8 +2,8 @@ require 'ticking_away'
 
 bot = Cinch::Bot.new do
         configure do |c|
-          c.server = server
-          c.channels = channels
+          c.server = 'irc.freenode.org'
+          c.channels = ['my_channel', 'my_channel_2']
           c.nick = 'TickingAwayBot'
           c.plugins.plugins = [TickingAway::TimeInfo]
           c.plugins.options[TickingAway::TimeInfo] = {
