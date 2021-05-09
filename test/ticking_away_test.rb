81ebba5b95c12a0606a4ba1c6b263a23a7b628ac
Jeff Wood <woodjeffrey2@gmail.com>
2021-05-09 12:02:48 -0700

    refactor mock storage

--- test/ticking_away_test.rb
+++ test/ticking_away_test.rb
@@ -1,31 +1,12 @@
 require 'test_helper'
 
 class TickingAwayTest < Minitest::Test
-
-  class MockStorage
-    attr_reader :file_name
-    attr_accessor :stats
-
-    def initialize(file_name)
-      @file_name = file_name
-      @stats = {}
-    end
-
-    def increment_stat(stat_name)
-      if stats[stat_name]
-        stats[stat_name] += 1
-      else
-        stats.merge!({ stat_name => 1 })
-      end
-    end
-
-    def get_stat(stat_name)
-      call_count = 0
-      stats.each do |key, value|
-        call_count += value if key.start_with?(stat_name)
-      end
-
-      call_count
+  # Mock storage to inject for tests. Different tests writing
+  # + reading with the same file names can step on each others
+  # toes and cause strange behavior
+  class MockStorage < TickingAway::JSONFileStorage
+    def save_stats
+      true # don't save anything to file
     end
   end
 end
